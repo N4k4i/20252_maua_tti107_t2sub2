ce7a7641056560d0854720131c0a768695441966
Isabela Simodo Nakai <bebelanakai@gmail.com>
2025-10-08 14:11:55 -0300

    Update variaveis.js

--- TTI107/07 javascript/variaveis.js
+++ TTI107/07 javascript/variaveis.js
@@ -1,35 +1,51 @@
-const a = 20;
-const nome = 'Andreia';
-console.log(a, nome);
+const a = 18;
+const nome = "Gabriel"
 
-// a = 12 constantes não admitem valores novos
+console.log(a, nome)
 
-var x = 10;
-var y = 7;
-console.log('x =' + x + "\ny = " + y);
+// a = 12 constantes nao admitem valores novos
 
-var x = 'Praça Maua'
-console.log("x = " + x)
+var x = 10
+var y = 7
+
+console.log('x = ' + x + '\ny = ' + y)
+
+var x = "Praça Mauá"
+
+console.log('x = ' + x)
+
+// var admite redeclaração  
 
 let i = 12.5
-console.log("i = " + i)
-// let i = 34 : let não permite redeclaração no mesmo contexto
 
-let m = 10;
-var p = 20;
-of (m > 2); {
+console.log('i = ' + i)
+
+// let i = 34 let não admite redeclaração no mesmo contexto
+
+let m = 10
+var p = 20 
+if (m > 2) {
     let m = 23;
     var p = 3;
-    console.log("dentro do if: \nm = " + m + "\np =" + p);
+    console.log ('dentro do if: \nm = ' + m + "\np = " + p )
 }
-console.log("saiu do if:\nm =" + m + "\np = " + p)
 
-let sobrenome = 'Machion'
-console.log(nome + '' + sobrenome)
-console.log(`meu nome é ${nome}`)
+console.log ("saiu do if: \nm =" + m + "\np = " + p)
 
-n1 = 2;
-n2 = '3';
-console.log(n1 + n2)
-n4 = n1 + Number(n2)
+let sobrenome = 'Araujo'
+console.log (nome + " " +sobrenome)
+
+console.log (`meu nome é ${nome}`)
+
+// coerção implícita (concatenação):
+const n1 = 2
+const n2 = '3'
+const n3 = n1 + n2;
+console.log(n3);
+
+// coerção explícita (soma):
+const n4 = n1 + Number (n2)
 console.log(n4)
+
+// a classe number é a 'evolução' da forma primitiva number
+//  NaN: not a number
