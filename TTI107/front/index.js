c11df3467439d962f5302411f188991ef1e21586
ISABELA SIMODO NAKAI <25.11733-4@maua.br>
2025-10-23 10:50:35 -0300

    mongodb

--- TTI107/front/index.js
+++ TTI107/front/index.js
@@ -1,16 +1,14 @@
 //aqui esta nossa string de conxeao com banco de dados
-//mongodb+srv://erick_ken:<db_password>@cluster0.uzkctku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+//mongodb+srv://nakaidbuser:54779274885@cluster01.ea7a9tr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01
 
 const express = require("express");
 const cors = require("cors")
+const mongoose = require('mongoose')
 const app = express();
 app.use(express.json());
 app.use(cors())
 
-//requisição GET no endereço http://localhost:3000/oi
-app.get("/oi", (req, res) => {
-  res.send("oi");
-});
+
 
 let filmes = [
   {
@@ -24,6 +22,17 @@ let filmes = [
       "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela",
   },
 ];
+
+//requisição GET no endereço http://localhost:3000/oi
+app.get("/oi", (req, res) => {
+  res.send("oi");
+});
+
+//função de conexão com o banco
+async function conectarAoMongoDB() {
+  await mongoose.connect('mongodb+srv://nakaidbuser:54779274885@cluster01.ea7a9tr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01')
+}
+
 
 //requisição GET para obter a lista de filmes: http://localhost:3000/filmes
 app.get("/filmes", (req, res) => {
@@ -43,4 +52,12 @@ app.post("/filmes", (req, res) => {
   res.send(filmes)
 })
 
-app.listen(3000, () => console.log("servidor up and running"));
+app.listen(3000, () => {
+  try {
+    conectarAoMongoDB()
+    console.log('server up & running')
+  }
+  catch (e) {
+    console.log('erro:' + e)
+  }
+})
