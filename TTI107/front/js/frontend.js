682ff6e9208a310463cd0d3b5cea5ea9e71d5662
Isabela Nakai <25.11733-4@maua.br>
2025-11-06 11:00:18 -0300

    faltou o catch error

--- TTI107/front/js/frontend.js
+++ TTI107/front/js/frontend.js
@@ -1,55 +1,52 @@
-frontend.js
-
-const protocolo = 'http://'
-const baseURL = 'localhost:3000'
-const filmesEndpoint = '/filmes'
+const protocolo = "http://";
+const baseURL = "localhost:3000";
+const filmesEndpoint = "/filmes";
 
 async function obtemFilmes() {
-    // console.log('teste');
-    const URLcompleta = $, { protocolo }, any, { baseURL } $, { filmesEndpoint }
-    const filmes = (await axios.get(URLcompleta)).data
+    //console.log("teste");
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`;
+    const filmes = (await axios.get(URLcompleta)).data;
     // console.log(filmes)
-    // posicionar sobre o elemento tabela pela sua classe filmes
-    let tabela = document.querySelector('.filmes')
-    // posicionar sobre o corpo da tabela pela sua tag
-    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
-    // para cada filme na lista de filmes, criar uma linha nova
+    //posicionar sobre o elemento tabela pela sua classe filmes
+    let tabela = document.querySelector(".filmes");
+    //posicionar sobre o corpo da tabela pela sua tag
+    let corpoTabela = tabela.getElementsByTagName("tbody")[0];
+    //para cada filme na lista de filmes, criar uma linha nova
     for (let filme of filmes) {
-        let linha = corpoTabela.insertRow(0)
-        let celulaTitulo = linha.insertCell(0)
-        let celulaSinopse = linha.insertCell(1)
-        celulaTitulo.innerHTML = filme.titulo
-        celulaSinopse.innerHTML = filme.sinopse
+        let linha = corpoTabela.insertRow(0);
+        let celulaTitulo = linha.insertCell(0);
+        let celulaSinopse = linha.insertCell(1);
+        celulaTitulo.innerHTML = filme.titulo;
+        celulaSinopse.innerHTML = filme.sinopse;
     }
 }
 
 async function cadastrarFilme() {
-    // montar a URL
-    const URLcompleta = $, { protocolo }, any, { baseURL }, $, { filmesEndpoint }
-    // pegar os dados que o usuário digitou
-    let tituloInput = document.querySelector('#tituloInput')
-    let sinopseInput = document.querySelector('#sinopseInput')
-    let titulo = tituloInput.value
-    let sinopse = sinopseInput.value
+    //montar a URL
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`;
+    //pegar os dados que o usuário digitou
+    let tituloInput = document.querySelector("#tituloInput");
+    let sinopseInput = document.querySelector("#sinopseInput");
+    let titulo = tituloInput.value;
+    let sinopse = sinopseInput.value;
 
     if (titulo && sinopse) {
-        // limpa as caixinhas de input 
-        tituloInput.value = ""
-        sinopseInput.value = ""
-        // postMessage(destino, dados)
-        // requisição post para o back que devolve a lista de filmes atualizada
-        const filmes = (await axios.post(URLcompleta, { titulo, sinopse })).data
-        // limpa o corpo da tabela
-        let tabela = document.querySelector(".filmes")
-        let corpoTabela = tabela.getElementsByTagName('tbody')[0]
-        corpoTabela.innerHTML = ""
-        // remontando a tabela
+        //limpa as caixinhas de input
+        tituloInput.value = "";
+        sinopseInput.value = "";
+        //requisição post para o back, que devolve a lista de filmes atualizada
+        const filmes = (await axios.post(URLcompleta, { titulo, sinopse })).data;
+        //limpa o corpo da tabela
+        let tabela = document.querySelector(".filmes");
+        let corpoTabela = tabela.getElementsByTagName("tbody")[0];
+        corpoTabela.innerHTML = "";
+        //remontando a tabela
         for (let filme of filmes) {
-            let linha = corpoTabela.insertRow(0)
-            let celulaTitulo = linha.insertCell(0)
-            let celulaSinopse = linha.insertCell(1)
-            celulaTitulo.innerHTML = filme.titulo
-            celulaSinopse.innerHTML = filme.sinopse
+            let linha = corpoTabela.insertRow(0);
+            let celulaTitulo = linha.insertCell(0);
+            let celulaSinopse = linha.insertCell(1);
+            celulaTitulo.innerHTML = filme.titulo;
+            celulaSinopse.innerHTML = filme.sinopse;
         }
     }
     else {
@@ -59,6 +56,54 @@ async function cadastrarFilme() {
         setTimeout(() => {
             alert.classList.add('d-none')
             alert.classList.remove('show')
-        })
+        }, 2000)
     }
 }
+
+async function cadastrarUsuario() {
+    //navegar na árvore DOM até os inputs
+    let usuarioCadastroInput = document.querySelector('#usuarioCadastroInput')
+    let passwordCadastroInput = document.querySelector('#passwordCadastroInput')
+    //captura os valores digitados
+    let usuarioCadastro = usuarioCadastroInput.value
+    let passwordCadastro = passwordCadastroInput.value
+    if (usuarioCadastro && passwordCadastro) {
+        //cadastrar usuario, utilizando controle de fluxos
+        try{
+            let cadastroEndpoint = '/singup'
+            let URLcompleta = '${protocolo}${baseurl}${cadastroEndpoint}'
+            await axios.post(URLcompleta, {login: usuarioCadastro, password: passwordCadastro})
+            //limpa as caixinhas
+            usuarioCadastroInput.value = ""
+            passwordCadastroInput.value = ""
+            let alert = document.querySelector('.alert-modal-cadastro')
+            alert.innerHTML = "Usuário cadastrado com sucesso!"
+            alert.classList.add('show', 'alert-success')
+            alert.classList.remove('d-none', alert-danger)
+            setTimeout(() => {
+                alert.classList.add('d-none')
+                alert.classList.remove('show')
+                let modalCadastro = bootstrap.Modal.getInstance(document.querySelector9('#modalCadastro'))
+                modalCadastro.hide()
+            }, 2000)
+            //fechar o modal
+        }
+        catch (erro) {
+
+        }
+    }
+    else {
+        //exibir alerta para digitar campus
+        let alert = document.querySelector('.alert-modal-cadastro')
+        alert.innterHTML = "Preencha todos os campos!"
+        alert.classList.add('show', 'alert-danger')
+        alert.classList.remove('d-none')
+        setTimeout(() => {
+            alert.classList.add('d-none')
+            alert.classList.remove('alert-danger')
+        }, 2000)
+    }
+}
+
+
+
--- backend.js
+++ backend.js
@@ -1,102 +1,110 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const dotenv = require("dotenv")
-const uniqueValidator = requre('mongoose-unique-validator')
+const dotenv = require('dotenv')
+const uniqueValidator = require('mongoose-unique-validator')
 const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
 const app = express()
-dotenv.config()
 app.use(express.json())
 app.use(cors())
+dotenv.config()
 
-const stringConexao = process.env.CONEXAO_BD
-async function conectarAoMongoDB() {
-    await mongoose.connect(stringConexao)
+const stringConexaoBD = process.env.CONEXAO_BD
+
+//função de conexão com o banco
+async function conectarAoMongoDB () {
+  await mongoose.connect(stringConexaoBD)
 }
 
-const Filme = mongoose.model("filme", mongoose.Schema({
-    titulo: { type: String },
-    sinopse: { type: String }
+const Filme = mongoose.model ("Filme", mongoose.Schema({
+  titulo: {type: String}, 
+  sinopse: {type: String}
 }))
 
 const usuarioSchema = mongoose.Schema({
-    login: { type: string, required: true },
-    password: { type: string, required: true }
+  login: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
 })
-
 usuarioSchema.plugin(uniqueValidator)
 const Usuario = mongoose.model("Usuario", usuarioSchema)
 
-
-app.get("/oi", (req, res) => {
-    res.send('olá')
-})
-
-app.listen(3000, () => {
-    try {
-        conectarAoMongoDB()
-        console.log("up and running")
-    }
-    catch (e) {
-        console.log("erro:" + e)
-    }
-})
-
-// cadastrar novo filme: post filmes; http://localhost.3000/filmes
-app.post('/filmes', async (req, res) => {
-    //montar um objetvo json filme com as ifnformaçõs recebidas
-    const titulo = req.body.titulo
-    const sinopse = req.body.sinopse
-    // construir um objeto da classe filme
-    const filme = new Filme({ titulo: titulo, sinopse: sinopse })
-    // enviar o filme p o banco 
-    await filme.save()
-    // buscar a coleção do banco atualizada 
-    const filmes = await Filme.find()
-    // só para verificar: envia a base aualizada
-    res.json(filmes)
-})
-
-app.post('/singup', async (req, res) => {
-    try {
-        const login = req.body.login
-        const password = req.body.password
-        //criptografar a senha
-        const passwordCriptografada = await bcrypt.hash(password, 10);
-        //constroi o objeto usuário
-        const usuario = new Usuario({
-            login: login,
-            password: passwordCriptografada
-        })
-        //salvar o usuário, capturando a resposto do Mongo
-        const respostaMongo = await usuario.save()
-        console.log(respostaMongo);
-        res.status(201).end()
-    }
-    catch (exception) {
-        console.log(exception)
-        res.status(409).end()
-    }
-})
-
-//  endpoint para atender um get filmes: http//localhost:3000/filmes
-
+//endpoint para atender um get filmes: http://localhost:3000/filmes
 app.get('/filmes', async (req, res) => {
     const filmes = await Filme.find()
     res.json(filmes)
 })
 
-// let filmes = [{
-//     titulo: "Forrest Gump - O Contador de Histórias",
-//     sinopse:
-//         "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções."
-// },
-// {
-//     titulo: "Um Sonho de Liberdade",
-//     sinopse:
-//         "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela"
-// }
-// ];
+//cadastrar um novo filme: post filmes: http://localhost:3000/filmes
+app.post('/filmes', async (req, res) => {
+    //capturar as informações do usuário
+    const titulo = req.body.titulo
+    const sinopse = req.body.sinopse
+    //construir um objeto filme da classe Filme
+    const filme = new Filme({titulo: titulo, sinopse: sinopse})
+    //salvar o filme no banco
+    await filme.save()
+    //buscar no banco a base atualizada
+    const filmes = await Filme.find()
+    res.json(filmes)
+})
+app.post('/signup', async (req, res) => {
+  try {
+    //captura o que o usuário digita
+    const login = req.body.login
+    const password = req.body.password
+    //criptografar a senha
+    const passwordCriptografada = await bcrypt.hash(password, 10)
+    //constroi o objeto usuário
+    const usuario = new Usuario({
+      login: login,
+      password: passwordCriptografada
+    })
+    //salvar o usuário, capturando a resposta do Mongo
+    const respostaMongo = await usuario.save()
+    console.log(respostaMongo);
+    res.status(201).end()
+  }
+  catch (exception) {
+    console.log(exception)
+    res.status(409).end()
+  }
+})
+
+app.post('/login', async (req, res) => {
+  //captura o que o usuário digita
+  const login = req.body.login
+  const password = req.body.password
+  //busca no banco MongoDB
+  const user = await Usuario.findOne({login: login})
+  if (!user) {
+    //usuário não encontrado, encerra com mensagem adequada
+    return res.status(401).json({mesagem: "usuário inválido"})
+  }
+  const senhaValida = await bcrypt.compare(password, user.password)
+  if (!senhaValida) {
+    //senha inválida, encerra com mensagem adequada
+    return res.status(401).json({mensagem: "senha inválida"})
+  }
+  //vamos gerar o token e dovolver
+  const token = jwt.sign(
+    {login: login},
+    "senha-secreta",
+    {expiresIn: "1h"}
+  )
+  res.status(200).json({token: token})
+})
+
+
+app.listen(3000, () => {
+    try {
+      conectarAoMongoDB()
+      console.log('server up & running & conexão ok')
+    }
+    catch (e) {
+      console.log("erro:" + e)
+    }
+})
 
 
 
